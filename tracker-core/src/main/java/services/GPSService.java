927083e034876bf5cfa7bb4acd82376b76ae5904
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-01-16 13:38:17 +0700

    засписание по крону

--- tracker-core/src/main/java/services/GPSService.java
+++ tracker-core/src/main/java/services/GPSService.java
@@ -3,6 +3,7 @@ package services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jdev.dto.PointDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +14,7 @@ public class GPSService {
     StorageService storageService;
 
     @PostConstruct
+    @Scheduled(cron = "${cron.prop.gps}")
     private  void init() throws JsonProcessingException, InterruptedException {
         storageService.put(getGPS());
     }
