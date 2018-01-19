a926ba110bc17335709b619f58e2704157c9bee9
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-01-19 11:52:44 +0700

    Revert "измерения в PointDTO

--- common/src/main/java/jdev/dto/PointDTO.java
+++ common/src/main/java/jdev/dto/PointDTO.java
@@ -11,21 +11,7 @@ public class PointDTO {
     private double azimuth;
     private int instaSpeed;
 
-    public int getInstaSpeed() {
-        return instaSpeed;
-    }
 
-    public void setInstaSpeed(int instaSpeed) {
-        this.instaSpeed = instaSpeed;
-    }
-
-    public double getAzimuth() {
-        return azimuth;
-    }
-
-    public void setAzimuth(double azimuth) {
-        this.azimuth = azimuth;
-    }
 
     public double getLat() {
         return lat;
